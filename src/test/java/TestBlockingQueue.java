a63390dbcf64fc838f760eac2c8927d68fc67620
houkai <hikoukay@163.com>
2019-12-16 23:58:21 +0800

    1

--- src/test/java/TestBlockingQueue.java
+++ src/test/java/TestBlockingQueue.java
@@ -10,12 +10,58 @@ public class TestBlockingQueue {
      * @param args
      */
     public static void main(String[] args) {
-        (new TestBlockingQueue()).test1();
-        (new TestBlockingQueue()).test2();
+        (new TestBlockingQueue()).test0();
+//        (new TestBlockingQueue()).test1();
+//        (new TestBlockingQueue()).test2();
     }
 
 
+    /**
+     * 测试阻塞队列
+     */
+    private void test0() {
+        //阻塞队列
+        BlockingQueue<Integer> blockingQueue = new LinkedBlockingDeque<>();
+        //第一个线程往里面加数
+        Thread T1= new Thread(()->{
+            for (int i = 0; i < 10; i++) {
+                //往队列里加数
+                blockingQueue.offer(i);
+            }
+        });
+        //第二个线程往里面加数
+        Thread T2= new Thread(()->{
+            for (int i = 10; i < 20; i++) {
+                //往队列里加数
+                blockingQueue.offer(i);
+            }
+        });
+        //第二个线程从里面取数
+        Thread T3= new Thread(()->{
+            while (true){
+                //从队列里取数并打印
+                try {
+                    Integer take = blockingQueue.take();
+                    System.out.println("消费数字; "+take);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
 
+        T1.start();
+        T2.start();
+        T3.start();
+
+        //执行完之后一起退出
+        try {
+            T1.join();
+            T2.join();
+            T3.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
     /**
      * 测试阻塞队列
      */
@@ -63,6 +109,7 @@ public class TestBlockingQueue {
         T2.start();
         T3.start();
 
+        //执行完之后一起退出
         try {
             T1.join();
             T2.join();
